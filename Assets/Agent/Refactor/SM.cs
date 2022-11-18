04b46dfc09978c19952f60dc2d6338e439bc6a10
Philip Cox <philipcox197@gmail.com>
2022-11-18 16:29:06 +0000

    skele code for refactor

---
+++ Assets/Agent/Refactor/SM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+namespace Assets.Agent.Refactor {
+    public class SM<T> where T : SMSensor {
+        State<T>[] states;
+        Input<T>[] inputs;
+
+        // Key being the source state
+        // Value being the input index with a corrosponding new state index
+        private Dictionary<int, (int, int)[]> transitions;
+
+        public SM() {
+            
+        }
+    }
+
+    public class Input<T> where T : SMSensor {
+        private string name;
+        private Func<T, bool> activation;
+
+        public Input(string name, Func<SMSensor, bool> activation) {
+            this.name = name;
+            this.activation = activation;
+        }
+    }
+
+    public class State<T> where T: SMSensor {
+        private string name;
+        private Action<T> act;
+
+        public State(string name, Action<T> act) {
+            this.name = name;
+            this.act = act;
+        }
+    }
+}
---
+++ Assets/Agent/Refactor/SMSensor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Agent.Refactor {
+    public class SMSensor : MonoBehaviour {
+        private int curState;
+        private int id;
+        private static int nextId = 0;
+
+        // Use this for initialization
+        void Start() {
+            id = nextId;
+            nextId++;
+        }        
+    }
+}
